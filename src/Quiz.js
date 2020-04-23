8925829308b779106485c8b44a7f666cc3dd31f2
jcheung <jcheung@travelers.com>
2020-04-23 15:10:11 -0400

    a

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json')
 
@@ -10,8 +11,12 @@ class Quiz extends Component {
     }
 
     render() {
+        if (quiz_position - 1 === quizData.quiz_question.length) {
+            const isQuizEnd = true;
+        }
         return (
             < div >
+                {isQuizEnd === true ? <QuizEnd /> : null}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div >
         )
