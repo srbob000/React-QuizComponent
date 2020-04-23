b29091be81630699528d7d6da9fc132cafe34f55
jcheung <jcheung@travelers.com>
2020-04-23 14:28:12 -0400

    a

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,9 +12,7 @@ class Quiz extends Component {
     render() {
         return (
             < div >
-                <div className="QuizQuestion">
-                    <QuizQuestion />
-                </div>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div >
         )
     }
