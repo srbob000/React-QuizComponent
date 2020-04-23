69ff29ecb21f7a745a89f2dc8ab5bc66e4e37947
jcheung <jcheung@travelers.com>
2020-04-23 15:22:38 -0400

    a

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,11 +11,10 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_question.length)
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             < div >
                 {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div >
         )
     }
