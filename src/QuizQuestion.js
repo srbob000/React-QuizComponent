fc7e71dbe02279779c3de4b4ae6feacd45e1d255
jcheung <jcheung@travelers.com>
2020-04-23 16:04:30 -0400

    a

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props)
+        this.state({ incorrectAnswer: false })
+    }
     handleClick(buttonText) {
+        this.setState(incorrectAnswer = false)
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState(incorrectAnswer = true)
             this.props.showNextQuestionHandler()
         }
     }
